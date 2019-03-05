3e830a7ae8a08a54df1d4b855f4e9ccaf9b54fde
crosenau <rosenauc74@hotmail.com>
2019-03-05 00:16:26 -0800

    first commit

---
+++ validation/login.js
@@ -0,0 +1,27 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  // Convert empty fields to an empty string so we can use Validator functions
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
+  // Email validation
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  // Password validation
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
